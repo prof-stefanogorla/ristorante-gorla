00436c65ae33756d9a666e325240c69b3d796e3b
Stefano Gorla <stefano.gorla@gmail.com>
2025-01-21 21:51:05 +0100

    class Ristorante

--- ristorante.py
+++ ristorante.py
@@ -18,3 +18,30 @@ class Menu:
         if not self.piatti:
             return "Il menu' e' vuoto."
         return "\n".join([str(piatto) for piatto in self.piatti])    
+
+class Ristorante:
+	def __init__(self, nome):
+		self.nome = nome
+		self.menu = Menu()
+	
+	def avvia(self):
+		while True:
+			print(f"\nBenvenuto al ristorante {self.nome}")
+			print("2. Mostra il menu'")
+			print("3. Esci")
+	
+			scelta = input("Scegli un'opzione: ")
+			if scelta == "1":
+				self.aggiungi_piatto()
+			elif scelta == "2":
+				print(self.menu.mostra_menu())
+			elif scelta == "3":
+				break
+			else:
+				print("Opzione non valida.")
+	
+	def aggiungi_piatto(self):
+		nome = input("Inserisci il nome del piatto: ")
+		prezzo = float(input("Inserisci il prezzo: "))
+		tipo = input("Inserisci il tipo: ")
+		self.menu.aggiungi_piatto(Piatto(nome, prezzo, tipo))
